510762740cebbf89c11fc611882a39a583f8c01f
Ayush Sahu <ayushsahu050502@gmail.com>
2023-10-21 02:11:16 +0530

    01

---
+++ greavease/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Forward from './LandingPage/Forward';
+import LandingPage from './LandingPage/LandingPage';
+import SignupPage from './SignUp/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <SignupPage/>
+    </div>
+  );
+}
+
+export default App;
