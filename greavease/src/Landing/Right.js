6eb619cc61c99b2e2b1581c7990cca04c6197ee0
Ayush Sahu <ayushsahu050502@gmail.com>
2023-10-22 15:35:09 +0530

    Button

--- greavease/src/Landing/Right.js
+++ greavease/src/Landing/Right.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 const Right = () => {
   const cardStyles = {
     position: "fixed",
@@ -19,41 +19,39 @@ const Right = () => {
   };
   const headingStyles = {
     fontSize: "55px", // Adjust the font size as needed
-    fontFamily: 'Oswald, sans-serif',
-    padding:"0",
-    margin:"2px 0",
-    
+    fontFamily: "Oswald, sans-serif",
+    padding: "0",
+    margin: "2px 0",
   };
-  const span1={
-    color:"#3E001F",
-  }
-  const span2={
-    color:"#6C3428"
-  }
-  const pstyle={
-    color:"#3D0C11",
-    fontSize:"25px",
-    margin: '12px 0',
-  }
+  const span1 = {
+    color: "#3E001F",
+  };
+  const span2 = {
+    color: "#6C3428",
+  };
+  const pstyle = {
+    color: "#3D0C11",
+    fontSize: "25px",
+    margin: "12px 0",
+  };
   const buttonStyles = {
-    backgroundColor: 'rgba(243,130,66,255)',
-    color: '#6C3428',
-    border: 'none',
-    borderRadius: '30px', // Add border radius
-    padding: '10px ',
-    cursor: 'pointer',
-    position: 'absolute',
-    bottom: '42px', // Moved to the bottom
-    left: '20px',
-    fontSize:"20px",
-    width:"200px",
+    backgroundColor: "rgba(243,130,66,255)",
+    color: "#6C3428",
+    border: "none",
+    borderRadius: "30px", // Add border radius
+    padding: "10px ",
+    cursor: "pointer",
+    position: "absolute",
+    bottom: "42px", // Moved to the bottom
+    left: "20px",
+    fontSize: "20px",
+    width: "200px",
   };
-  buttonStyles[':hover'] = {
-    backgroundColor: 'darkblue', 
-   
+  buttonStyles[":hover"] = {
+    backgroundColor: "darkblue",
   };
   const arrowStyles = {
-    fontSize: '30px', // Adjust the size of the arrow as needed
+    fontSize: "30px", // Adjust the size of the arrow as needed
     // marginLeft: '10px',
     // position: 'absolute',
     // color: '#6C3428',
@@ -66,7 +64,11 @@ const Right = () => {
   };
   return (
     <div style={cardStyles}>
-      <h1 style={headingStyles}><span style={span1}>Grievances</span> <span style={span2}>becomes solutions </span><span style={span1}>here</span></h1>
+      <h1 style={headingStyles}>
+        <span style={span1}>Grievances</span>{" "}
+        <span style={span2}>becomes solutions </span>
+        <span style={span1}>here</span>
+      </h1>
       <p style={pstyle}>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -74,13 +76,12 @@ const Right = () => {
         scrambled it to make a type specimen book.{" "}
       </p>
       <span>
-        <Link to="/signin"><button style={buttonStyles} onClick={handleButtonClick} >Get Started<span style={arrowStyles}>&rarr;</span></button>
-        
+        <Link to="/signin">
+          <button style={buttonStyles} onClick={handleButtonClick}>
+            Get Started<span style={arrowStyles}>&rarr;</span>
+          </button>
         </Link>
-        </span>
-        
-
-      
+      </span>
     </div>
   );
 };
