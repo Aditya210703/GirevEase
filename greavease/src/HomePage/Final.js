4530bfa49c3304341cbd3bb983a4ef6ff4875445
Aditya210703 <vermaaditya2356@gmail.com>
2023-10-24 15:35:57 +0530

    Updated-Homepage

--- greavease/src/HomePage/Final.js
+++ greavease/src/HomePage/Final.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "../HomePage/Home1/Home1";
-// import Header from "../component/Header";
 import Header from "./home/header";
-const Final=()=>{
-    return(
-        <div>
-            <Header></Header>
-            <Home></Home>
-        </div>
-    )
-}
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
+const Final = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+        // Redirect to the '/Signin' route when user is not authenticated
+        navigate('/Signin', { replace: true });
+      }
+    });
+
+    return () => {
+      // Unsubscribe from the auth state change listener when the component unmounts
+      unsubscribe();
+    };
+  }, [navigate]);
+
+  if (!isAuthenticated) {
+    // This part is for rendering a loading component or message
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
 
 export default Final;
